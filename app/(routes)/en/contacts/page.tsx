9db2014ffa1c64662b3e02559cf9ee5da7a4ec81
bach9w <78446671+bach9w@users.noreply.github.com>
2023-12-20 16:12:15 +0200

    Refactor code for improved performance

---
+++ app/(routes)/en/contacts/page.tsx
@@ -0,0 +1,35 @@
+'use client';
+
+import Container from '@/components/ui/container';
+import { motion } from 'framer-motion';
+import MyGoogleMap from '@/components/googleMaps/map';
+import Component from './components/email-form';
+
+const ContactPage = () => {
+	return (
+		<motion.div
+			initial={{ opacity: 0, scale: 0.5 }}
+			animate={{ opacity: 1, scale: 1 }}
+			transition={{ duration: 0.5 }}
+		>
+			<Container>
+				<div className="text-center  justify-center object-center w-full">
+					<div className="bg-black text-white mb-1">
+						<h1>Контакти</h1>
+					</div>
+					<div className="object-center w-full mt-2 justify-center flex">
+						<MyGoogleMap />
+					</div>
+					<div className="bg-black text-white mt-2 text-sm md:text-xl grid-col-1 text-center object-center justify-center">
+						<div className="flex justify-center mb-2">
+							Адрес - гр. Банско - Тома Вишанов 3
+						</div>
+					</div>
+				</div>
+				<Component />
+			</Container>
+		</motion.div>
+	);
+};
+
+export default ContactPage;
---
+++ app/(routes)/en/layout.tsx
@@ -0,0 +1,44 @@
+import type { Metadata } from 'next';
+import { Urbanist } from 'next/font/google';
+import { Sofia_Sans } from 'next/font/google';
+import '@/app/globals.css';
+import Footer from '@/components/footer';
+import Navbar from '@/components/navbar';
+import ModalProvider from '@/providers/modal-provider';
+import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
+
+const font = Sofia_Sans({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+	title: 'Zasheva - Bansko',
+	description: 'Zasheva house - Bansko',
+};
+
+export default function RootLayout({
+	children,
+}: {
+	children: React.ReactNode;
+}) {
+	return (
+		<html lang="en">
+			<body
+				className={font.className + ' min-h-screen flex flex-col'}
+				style={{
+					backgroundImage: `url("/background.png")`,
+					backgroundSize: 'cover',
+					backgroundPosition: 'center',
+					height: '100%',
+				}}
+			>
+				<Analytics />
+				<SpeedInsights />
+				<Navbar />
+				<ModalProvider />
+
+				{children}
+				<Footer />
+			</body>
+		</html>
+	);
+}
