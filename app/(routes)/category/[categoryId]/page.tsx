1bbbca60cf3b0740155656546a1e97ac9834b304
bach9w <78446671+bach9w@users.noreply.github.com>
2024-02-07 17:25:11 +0200

    Refactor imports and update grid layout in category page

--- app/(routes)/category/[categoryId]/page.tsx
+++ app/(routes)/category/[categoryId]/page.tsx
@@ -1,13 +1,13 @@
-import getCategory from '@/actions/get-category';
-import getProducts from '@/actions/get-products';
-import getSizes from '@/actions/get-sizes';
-import Billboard from '@/components/billboard';
-import Container from '@/components/ui/container';
-import Filter from './components/filter';
-import NoResults from '@/components/ui/no-results';
-import ProductCard from '@/components/ui/product-card';
-import MobileFilters from './components/mobile-filters';
-import { motion } from 'framer-motion';
+import getCategory from "@/actions/get-category";
+import getProducts from "@/actions/get-products";
+import getSizes from "@/actions/get-sizes";
+import Billboard from "@/components/billboard";
+import Container from "@/components/ui/container";
+import Filter from "./components/filter";
+import NoResults from "@/components/ui/no-results";
+import ProductCard from "@/components/ui/product-card";
+import MobileFilters from "./components/mobile-filters";
+import { motion } from "framer-motion";
 
 interface CategoryPageProps {
 	params: {
@@ -43,7 +43,7 @@ const CategoryPage: React.FC<CategoryPageProps> = async ({
 						</div>
 						<div className="mt-6 lg:col-span-4 lg:mt-0">
 							{product.length === 0 && <NoResults />}
-							<div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+							<div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
 								{product.map((item) => (
 									<ProductCard key={item.id} data={item} />
 								))}
