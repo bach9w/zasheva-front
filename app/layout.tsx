fb486aed603df9b4d9a63e0f4198c084524d8e29
bach9w <78446671+bach9w@users.noreply.github.com>
2024-05-27 18:23:04 +0300

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,24 +5,21 @@ import "./globals.css";
 
 import { ReactNode } from "react";
 import Header from "@/components/Header/Header";
-import ConvexClientProvider from "../providers/ConvexClientProvider";
 
 const font = Sofia_Sans({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-	title: "Zasheva - Bansko",
-	description: "Zasheva house - Bansko",
+  title: "Zasheva - Bansko",
+  description: "Zasheva house - Bansko",
 };
 
 export default function RootLayout({ children }: { children: ReactNode }) {
-	return (
-		<html lang="en">
-			<body>
-				<ConvexClientProvider>
-					<Header />
-					{children}
-				</ConvexClientProvider>
-			</body>
-		</html>
-	);
+  return (
+    <html lang="en">
+      <body>
+        <Header />
+        {children}
+      </body>
+    </html>
+  );
 }
